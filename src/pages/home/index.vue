be20d7f0d5d2beb8a17ede2f35cc9f3ec485f43b
yangximin <yangximin@maoye.cn>
2023-02-23 17:46:51 +0800

    首页布局

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,15 +1,36 @@
 <template>
   <div>
-      我是首页
+    <TypeNav />
+    <ListContainer />
+    <Recommend />
+    <Rank />
+    <Like />
+    <Floor />
+    <Floor />
+    <Brand />
   </div>
 </template>
 
 <script>
+// import TypeNav from "@/components/TypeNav";
+import ListContainer from "./listContainer";
+import Recommend from "./recommend";
+import Rank from "./rank";
+import Like from "./like";
+import Floor from "./floor";
+import Brand from "./brand";
 export default {
-   name:'home'
-}
+  name: "home",
+  components: {
+    ListContainer,
+    Recommend,
+    Rank,
+    Like,
+    Floor,
+    Brand,
+  },
+};
 </script>
 
 <style>
-
 </style>
